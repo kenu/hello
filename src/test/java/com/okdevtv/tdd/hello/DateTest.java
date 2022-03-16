09057d48e5cf13e849f58f7a3ab695dd27f8f183
kenu <kenu.heo@gmail.com>
2022-03-16 11:30:01 +0900

    🐛 enabled #4

--- src/test/java/com/okdevtv/tdd/hello/DateTest.java
+++ src/test/java/com/okdevtv/tdd/hello/DateTest.java
@@ -9,11 +9,11 @@ import org.junit.Test;
 public class DateTest {
 
 	@Test
-	public void test() {
+	public void testDate() {
 		Date date = new Date();
 		Class<? extends Date> class1 = date.getClass();
 		String name = class1.getName();
-		assertEquals("", name);
+		assertEquals("java.util.Date", name);
 	}
 
 }
